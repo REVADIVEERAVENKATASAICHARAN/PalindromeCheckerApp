966d64f1f3762fd198cae46dded910b7ad013609
REVADIVEERAVENKATASAICHARAN <revadisaicharan@gmail.com>
2026-02-27 15:19:32 +0530

    UC5

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,42 +1,30 @@
-/**
- * ======================================================
- * MAIN CLASS - UseCase4PalindromeCheckerApp
- * ======================================================
- *
- * Use Case 4: Character Array Based Validation
- *
- * Description:
- * This class validates a palindrome by converting
- * the string into a character array and comparing
- * characters using the two-pointer technique.
- *
- * @author Developer
- * @version 4.0
- */
+import java.util.Stack;
 
 public class PalindromeCheckerApp {
 
-    /**
-     * Application entry point for UC4.
-     *
-     * @param args Command-line arguments
-     */
+
     public static void main(String[] args) {
 
-        String input = "radar";  // Hardcoded input
-        char[] characters = input.toCharArray();
+        // Declare and initialize the input string
+        String input = "noon";
 
-        int start = 0;
-        int end = characters.length - 1;
+        // Create a Stack to store characters
+        Stack<Character> stack = new Stack<>();
+
+        // Push each character of the string into the stack
+        for (char c : input.toCharArray()) {
+            stack.push(c);
+        }
+
+        // Assume palindrome initially
         boolean isPalindrome = true;
 
-        while (start < end) {
-            if (characters[start] != characters[end]) {
+        // Compare original string with popped characters
+        for (char c : input.toCharArray()) {
+            if (c != stack.pop()) {
                 isPalindrome = false;
                 break;
             }
-            start++;
-            end--;
         }
 
         System.out.println("Input : " + input);
