a4c33f9cbaf6e00458d07903482b2156ae61a4b4
REVADIVEERAVENKATASAICHARAN <revadisaicharan@gmail.com>
2026-02-27 14:55:23 +0530

    UC1

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,5 +1,11 @@
-public class HelloWorld {
+public class PalindromeCheckerApp{
+
     public static void main(String[] args) {
-        System.out.println("Hello, World!");
+
+        System.out.println("=====================================");
+        System.out.println("  Welcome to Palindrome Checker App ");
+        System.out.println("=====================================");
+        System.out.println("Version: 1.0");
+        System.out.println("Application started successfully!");
     }
 }
