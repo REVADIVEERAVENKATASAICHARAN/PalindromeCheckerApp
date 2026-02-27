4b767b137d4b4c3e09dfa44c19782754e384377c
REVADIVEERAVENKATASAICHARAN <revadisaicharan@gmail.com>
2026-02-27 15:00:56 +0530

    UC2

--- src/PalindromeCheckerApp.java
+++ src/PalindromeCheckerApp.java
@@ -1,11 +1,28 @@
-public class PalindromeCheckerApp{
+public class PalindromeCheckerApp {
 
     public static void main(String[] args) {
 
-        System.out.println("=====================================");
-        System.out.println("  Welcome to Palindrome Checker App ");
-        System.out.println("=====================================");
-        System.out.println("Version: 1.0");
-        System.out.println("Application started successfully!");
+        String word = "madam";  // Hardcoded string
+        boolean isPalindrome = true;
+
+        int left = 0;
+        int right = word.length() - 1;
+
+        while (left < right) {
+            if (word.charAt(left) != word.charAt(right)) {
+                isPalindrome = false;
+                break;
+            }
+            left++;
+            right--;
+        }
+
+        System.out.println("Word: " + word);
+
+        if (isPalindrome) {
+            System.out.println("Result: It is a Palindrome ✅");
+        } else {
+            System.out.println("Result: It is NOT a Palindrome ❌");
+        }
     }
 }
