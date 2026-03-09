3a7a074067cb99ad84c4dc95695803369f7500ca
REVADIVEERAVENKATASAICHARAN <revadisaicharan@gmail.com>
2026-03-09 15:28:16 +0530

    UC10

--- src/main/java/org/example/PalindromeCheckerApp.java
+++ src/main/java/org/example/PalindromeCheckerApp.java
@@ -1,28 +1,24 @@
 public class PalindromeCheckerApp {
 
+public static void main(String[] args) {
 
-    public static void main(String[] args) {
+    String input = "A man a plan a canal Panama";
 
-        String input = "madam";
+    // Normalize the string: remove non-letters and convert to lowercase
+    String normalized = input.replaceAll("[^a-zA-Z]", "").toLowerCase();
 
-        boolean result = check(input, 0, input.length() - 1);
+    boolean isPalindrome = true;
 
-        System.out.println("Input : " + input);
-        System.out.println("Is Palindrome? : " + result);
+    // Compare characters from both ends
+    for (int i = 0; i < normalized.length() / 2; i++) {
+
+        if (normalized.charAt(i) != normalized.charAt(normalized.length() - 1 - i)) {
+            isPalindrome = false;
+            break;
+        }
     }
 
-
-    private static boolean check(String s, int start, int end) {
-
-
-        if (start >= end) {
-            return true;
-        }
-
-        if (s.charAt(start) != s.charAt(end)) {
-            return false;
-        }
-
-        return check(s, start + 1, end - 1);
-    }
+    System.out.println("Input : " + input);
+    System.out.println("Is Palindrome? : " + isPalindrome);
+}
 }
